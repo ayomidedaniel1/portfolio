24cc1eca15c3dbfea687d6206d09f84f95ebc4b6
Ayomide <ayomidedans@gmail.com>
2022-03-24 01:01:57 +0100

    fixed contact form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
+
+import { Navbar, About, Contact } from './components';
+import { Hero, Skills, Portfolio, Footer } from './containers';
+
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='home' id='home'>
+        <Navbar />
+        <Hero />
+      </div>
+      <div className='grad__abt'>
+        <About />
+        <Skills />
+      </div>
+      <Portfolio />
+      <Contact />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
